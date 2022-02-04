f8e51e3dc1684e46f8fe58f1049da09ac7e68804
sherlock59 <97972071+sherlock59@users.noreply.github.com>
2022-02-04 09:53:59 -0600

    added the form

--- Salary_calc.assign./Salary_calc.py
+++ Salary_calc.assign./Salary_calc.py
@@ -3,7 +3,7 @@
 (''' Description: This project calculates invoice total with discount included based on user's input'
  * Written by: Anaberdi Meredov
  * Date Written: 2022.01.31
- * GitHub: https://github.com/sherlock59/InvoiceApp.git ''')
+ * GitHub:  ''')
 
 
 print("Welcome to the Salary Hours Calculator") # displays a welcome message
@@ -11,16 +11,16 @@ print("This calculator allows the user to determine hours worked for a whole yea
 print("")
 
 # get input from the user
-miles_driven= float(input("Enter miles driven:\t\t"))
-gallons_used = float(input("Enter gallons of gas used:\t"))
+salary_amount = float(input("Enter salary for per hour:\t\t"))
+hours_worked = float(input("Enter hours worked:\t"))
 
 # calculate and round miles per gallon
-mpg = miles_driven / gallons_used
+mpg = salary_amount / hours_worked
 mpg = round(mpg, 2)
 
 # display the result
 print()
-print("Miles Per Gallon:\t\t{mpg}")
+print("Salary for per year:\t\t{mpg}")
 print()
 print("Bye!")
 
