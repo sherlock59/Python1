05df320c60358ec5ce417bfb7a74730553d65c2d
sherlock59 <97972071+sherlock59@users.noreply.github.com>
2022-04-10 20:15:35 -0500

    ADDED NEW PYTHON PROJECT INTO GITHUB PYTHON FOLDER WHERE WILL BE KEPT WITH OTHER ASSIGNMENT FOLDERS

--- TempCalc/TempCalc.py
+++ TempCalc/TempCalc.py
@@ -5,3 +5,11 @@ LINE_LENGTH = 50
 print("Welcome to the Temperature Calculator")
 print('=' * LINE_LENGTH)
 
+while True:
+
+    while True:
+        monthly_investment = float(input("Enter monthly investment:\t"))
+        if 0 < monthly_investment <= 1000:
+            break
+        else:
+            print("Entry must be greater than 0. Please try again.")
