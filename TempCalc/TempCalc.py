844351c616794a00d6df39920fa216f794ebeff9
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-04-29 08:23:00 -0500

    asdsa

--- TempCalc/TempCalc.py
+++ TempCalc/TempCalc.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 
-(''' Description: This project works as calculator and allows users to convert temperature from C to F and F to C'
- * Written by: Anaberdi Meredov
- * Date Written: 2022.04.20
- * GitHub: https://github.com/sherlock59/Python1.git ''')
+'''Description: This project works as calculator and allows users to convert temperature from C to F and F to C'
+   Written by: Anaberdi Meredov
+   Date Written: 2022.04.20
+   GitHub: https://github.com/sherlock59/Python1.git'''
 
 LINE_LENGTH = 50
 # display a welcome message
 print("Welcome to the Temperature Calculator")
 print('=' * LINE_LENGTH)
+
 # global while loop
 while True:
     # this while loop validates if the user enters c or f and converts to small letter if typed with capital letters
     while True:
-        user_input = str(input("Enter F to convert C or enter C to convert to F: "))
+        user_input = input("Enter F to convert C or enter C to convert to F: ")
         conversion_type = user_input[0].lower()
         if conversion_type in ['f', 'c']:
             break
         else:
             print("Invalid letter entered! Please try again to convert the desired temperature...")
+
 #  Three while loops below examines if user typed legitimate numbers for calculation
     while True:
         temp_start = int(input(f'{"Enter the starting temperature (-50 to 150)":.<45s}:'))
         if -50 <= temp_start <= 150:
             break
         else:
-            print("invalid... Please try again")
+            print("Please try again, valid numbers are (-50 to 150) ")
 
     while True:
         temp_stop = int(input(f'{"Enter the stopping temperature (-50 to 150)":.<45s}:'))
@@ -40,13 +42,15 @@ while True:
             break
         else:
             print("Please provide a valid number")
+
     # for loop does conversion and presents them in an organized manner
     for temp in range(temp_start, temp_stop + 1, temp_step):
         if conversion_type == 'f':
             conversion_result = (temp - 32) * 5 / 9
         if conversion_type == 'c':
             conversion_result = temp * 9 / 5 + 32
-        print(f'| {temp:3.0f} | {conversion_result:3.0f} |')
+        print(f'| {conversion_type:3.0f} {temp:3.0f} | {conversion_result:3.0f} |')
+
     # this while loop validates if the user wants to convert another temperature
     while True:
         print('=' * LINE_LENGTH)
@@ -60,5 +64,6 @@ while True:
         break
     else:
         print()
+
 print('=' * LINE_LENGTH)
 print('Good bye')
