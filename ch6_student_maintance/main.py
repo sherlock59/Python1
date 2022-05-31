1823647b86da4e827090d830fbb60500b569074c
Meredov <97972071+sherlock59@users.noreply.github.com>
2022-05-30 19:50:57 -0500

    created files and removed confusing codes that I wrote

---
+++ ch6_student_maintance/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Student Data System for managing student information (student id, first name, and last name)
+This module contains the functions for displaying the main menu and running the menu options
+"""
+
+import student_maint
+
+__author__ = 'Anaberdi Meredov'
+__version__ = '2.0'
+__copyright__ = "Copyright 2022.05.12, Chapter 6 assigment"
+__github__ = "https://github.com/sherlock59/Python1.git"
